b0e500f62a88ddfce0e5403e13eb5162f9249078
bodrovis <bodrovis@protonmail.com>
2017-11-29 18:22:03 +0300

    initial

---
+++ lib/demo_web/router.ex
@@ -0,0 +1,33 @@
+defmodule DemoWeb.Router do
+  use DemoWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug SetLocale, gettext: DemoWeb.Gettext, default_locale: "ru"
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", DemoWeb do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+  end
+
+  scope "/:locale", DemoWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", DemoWeb do
+  #   pipe_through :api
+  # end
+end
